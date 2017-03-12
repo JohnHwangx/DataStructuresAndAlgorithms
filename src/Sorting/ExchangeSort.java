ddce06d2f62179a878c4b96742b776c1ac1428fe
JohnHwangx <johnhuangx@outlook.com>
2017-03-12 23:31:25 +0800

    add quick sort

--- src/Sorting/ExchangeSort.java
+++ src/Sorting/ExchangeSort.java
@@ -24,6 +24,7 @@ public class ExchangeSort {
 	}
 
 	public static <E extends Comparable<E>> void bubbleSort(E[] array) {
+		System.out.println("Bubble Sort:");
 		int i, j;
 		boolean exchange;
 		E temp;
@@ -42,5 +43,71 @@ public class ExchangeSort {
 			}
 			PrintOperator.Print(array);
 		}
+	}
+
+	public static void QuickSort(Integer[] A) {
+		System.out.println("Quick Sort:");
+		int low = 0, high = A.length - 1;
+		QuickSort(A, low, high);
+	}
+
+	private static void QuickSort(Integer[] a, int low, int high) {
+		// TODO Auto-generated method stub
+		if (low < high) {
+			int pivot = GetPosition(a, low, high);
+			QuickSort(a, low, pivot - 1);
+			QuickSort(a, pivot + 1, high);
+		}
+		PrintOperator.Print(a);
+	}
+
+	private static int GetPosition(Integer[] A, int low, int high) {
+		int pivot = A[low];
+		while (low < high) {
+			while (low < high && A[high] >= pivot) {
+				--high;
+			}
+			A[low] = A[high];
+			
+			while (low < high && A[low] <= pivot) {
+				++low;
+			}
+			A[high] = A[low];
+		}
+		A[low] = pivot;
+		return low;
+	}
+	
+	public static<E extends Comparable<E>> void quickSort(E[] array) {
+		System.out.println("Quick Sort:");
+		int low=0,high=array.length-1;
+		quickSort(array,low,high);
+	}
+
+	private static<E extends Comparable<E>> void quickSort(E[] array, int low, int high) {
+		// TODO Auto-generated method stub
+		if (low<high) {
+			int pivot=getPosition(array, low, high);
+			quickSort(array, low, pivot-1);
+			quickSort(array, pivot+1, high);
+		}
+		PrintOperator.Print(array);
+	}
+
+	private static<E extends Comparable<E>> int getPosition(E[] array, int low, int high) {
+		// TODO Auto-generated method stub
+		E pivot=array[low];
+		while (low<high) {
+			while (low<high&&array[high].compareTo(pivot)>=0) {
+				--high;
+			}
+			array[low]=array[high];
+			while (low<high&&array[low].compareTo(pivot)<=0) {
+				++low;
+			}
+			array[high]=array[low];
+		}
+		array[low]=pivot;
+		return low;
 	}
 }
--- src/Sorting/MainTest.java
+++ src/Sorting/MainTest.java
@@ -9,7 +9,7 @@ public class MainTest {
 		// TODO Auto-generated method stub
 		System.out.println("Before Sort:");
 		Integer[] A = { 49, 38, 65, 97, 76, 13, 27, 49 };
-		Character[] characters={'a','b','j','d','l','e'};
+		Character[] characters={'a','b','j','d','l','e','h','q','n','l'};
 		PrintOperator.Print(A);
 		
 		System.out.println("After sort:");
@@ -17,10 +17,12 @@ public class MainTest {
 //		InsertSort.InsertionSort(A);//Straight Insert Sort
 //		InsertSort.insertionSort(characters);//Straight Insert Sort with Generic
 //		InsertSort.ShellSort(A);//Shell Sort
-		InsertSort.shellSort(characters);//Shell sort with Generic
+//		InsertSort.shellSort(characters);//Shell sort with Generic
 		
 //		ExchangeSort.BubbleSort(A);//Bubble Sort
 //		ExchangeSort.bubbleSort(characters);
+//		ExchangeSort.QuickSort(A);
+		ExchangeSort.quickSort(characters);
 		
 //		SelectSort.SelectionSort(A);
 //		SelectSort.selectionSort(characters);
