8f097787d06c7924b920902de421098ff611032e
huangqiang <johnhuangx@outlook.com>
2017-04-16 14:10:41 +0800

    searching2

--- src/searching/SymbolTable.java
+++ src/searching/SymbolTable.java
@@ -1,5 +1,64 @@
 package searching;
 
-public class SymbolTable {
+import java.util.Iterator;
+import java.util.TreeMap;
 
+public class SymbolTable<Key, Value> implements Iterable<Key> {
+	private TreeMap<Key, Value> st;
+
+	public SymbolTable() {
+		// TODO create a symbol table
+		st = new TreeMap<Key, Value>();
+	}
+
+	public void put(Key key, Value value) {
+		// TODO put key-value pair into the table
+		// TODO (remove key from table if value is null)
+		if (key == null)
+			throw new IllegalArgumentException("called put() with null key");
+		if (value == null)
+			st.remove(key);
+		else
+			st.put(key, value);
+
+	}
+
+	public Value get(Key key) {
+		// TODO value paired with key
+		// TODO (null if key is absent)
+		if (key == null)
+			throw new IllegalArgumentException("called get() with null key");
+		return st.get(key);
+	}
+
+	public void delete(Key key) {
+		// TODO remove key (and its value) from table
+		put(key, null);
+	}
+
+	public boolean contains(Key key) {
+		// TODO is there a value paired with key?
+		return get(key) != null;
+	}
+
+	public boolean isEmpty() {
+		// TODO is the table empty?
+		return size() == 0;
+	}
+
+	public int size() {
+		// TODO number of key-value pairs in the table
+		return 0;
+	}
+
+	public Iterable<Key> keys() {
+		// TODO all the keys in the table
+		return st.keySet();
+	}
+
+	@Override
+	public Iterator<Key> iterator() {
+		// TODO Auto-generated method stub
+		return st.keySet().iterator();
+	}
 }
